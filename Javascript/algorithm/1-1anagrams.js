eb37d43a3956a06d192ddd582cb896bc71a9c205
Hakseong-Lee <83964261+Hakseong-Lee@users.noreply.github.com>
2023-01-24 22:53:46 +0900

    Add: algorithm 1-1anagrams

---
+++ Javascript/algorithm/1-1anagrams.js
@@ -0,0 +1,17 @@
+// validAnagram이라는 함수에 string 두개를 주면 두개의 구성 요소가 같은지 비교하여 참인지 거짓인지 리턴하는 함수를 만들어라
+// Frequency Counter - validAnagram
+// Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+// Examples:
+
+// validAnagram('', '') // true
+// validAnagram('aaz', 'zza') // false
+// validAnagram('anagram', 'nagaram') // true
+// validAnagram("rat","car") // false) // false
+// validAnagram('awesome', 'awesom') // false
+// validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana') // false
+// validAnagram('qwerty', 'qeywrt') // true
+// validAnagram('texttwisttime', 'timetwisttext') // true
+// Note: You may assume the string contains only lowercase alphabets.
+
+// Time Complexity - O(n)
