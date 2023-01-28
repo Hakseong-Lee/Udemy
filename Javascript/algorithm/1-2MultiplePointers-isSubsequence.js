837792abe851f87f84bc53523da7e5a2a58026dd
Hakseong-Lee <83964261+Hakseong-Lee@users.noreply.github.com>
2023-01-28 18:08:51 +0900

    Add: js algorithm Multiple Pointers - isSubsequence

---
+++ Javascript/algorithm/1-2MultiplePointers-isSubsequence.js
@@ -0,0 +1,27 @@
+// Multiple Pointers - isSubsequence
+// Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+
+// Examples:
+
+// isSubsequence('hello', 'hello world'); // true
+// isSubsequence('sing', 'evsting'); // true
+// isSubsequence('abc', 'abracadabra'); // true
+// isSubsequence('abc', 'acb'); // false (order matters)
+// Your solution MUST have AT LEAST the following complexities:
+
+// Time Complexity - O(N + M)
+
+// Space Complexity - O(1)
+
+function isSubsequence(str1, str2) {
+  let num = 0;
+  for (let i in str2) {
+    if (str1[num] == str2[i]) {
+      num++;
+    }
+    if (num === str1.length) {
+      return true;
+    }
+  }
+  return false;
+}
